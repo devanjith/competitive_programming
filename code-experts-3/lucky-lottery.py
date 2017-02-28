0087eb6d0bcbbeb3b575d5d2eb38559513482d19
devzdesilva <devzdesilva@gmail.com>
2017-02-28 11:15:43 +0530

    added lucky-lottery.py

---
+++ code-experts-3/lucky-lottery.py
@@ -0,0 +1,14 @@
+from itertools import permutations;
+_ = map(int, raw_input().split())
+n, t, tc = _[0], _[1], 0;
+perm_string = '1'*t + '0'*(n-t);
+for i in range(int(perm_string, 2)+1):
+    bi = bin(i)[2:].zfill(n);
+    if bi.count('1') == t:
+        #print bi;
+        c = 0;
+        c += 9**(bi[1:].count('0'));
+        if bi[0] == '0': c*=8;
+        tc += c;
+
+print tc;
